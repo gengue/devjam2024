fbc5e98e1883092264cad54cdb7dddc93ab7f448
Genesis Guerrero <genesis.guerrero@venturatravel.org>
2024-10-25 11:44:25 -0500

    add shadcn components and fix version of nextjs and clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <main>
-        <h1>Hello</h1>
-      </main>
-    </div>
+    <main>
+      <h1>Hello</h1>
+    </main>
   );
 }
