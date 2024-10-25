eb86e3db073543139ca839a6d80d4133afbace0c
jesus garcia <jesus.garcia@venturatravel.org>
2024-10-25 13:55:06 -0500

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import Image from "next/image";
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  if (userId) {
+    if (userId === 'user_2nwG70hg9e4mDYqou0vrHlJdaTJ') {
+      revalidatePath('/app/p') // Update cached posts
+      redirect(`/app/p`)
+    } else {
+      revalidatePath('/app/') // Update cached posts
+      redirect(`/app/`)
+    }
+
+  }
   return (
     <main>
       <h1>Hello</h1>
